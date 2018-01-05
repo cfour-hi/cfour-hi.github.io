da154bc4c3b86fc55200147b8784c6c418a5670f
monine <bob.zhouxiang@gmail.com>
2018-01-05 15:39:33 +0800

    update

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -81,7 +81,8 @@ const webpackConfig = merge(baseWebpackConfig, {
         // https://github.com/kangax/html-minifier#options-quick-reference
       },
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: 'dependency'
+      chunksSortMode: 'dependency',
+      env: JSON.parse(env.NODE_ENV)
     }),
     // keep module.id stable when vender modules does not change
     new webpack.HashedModuleIdsPlugin(),
