744fe237141ec1f1363e347c53fa92d59bab0f3c
monine <bob.zhouxiang@gmail.com>
2018-03-28 15:00:25 +0800

    lossed file

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,7 +36,8 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
-      '@comp': resolve('src/components')
+      '@comp': resolve('src/components'),
+      '@mixin': resolve('src/mixins')
     }
   },
   module: {
